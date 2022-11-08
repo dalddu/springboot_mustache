db35a4db50e33a8f477c8ed760483749b961712d
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-11-08 15:42:50 +0900

    list추가

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -4,31 +4,64 @@ import com.mustache.bbs.repository.ArticleRepository;
 import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.domain.entity.Article;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
 @Slf4j
 public class ArticleController {
 
-    @Autowired
-    private ArticleRepository articleRepository;
+    private final ArticleRepository articleRepository;
 
-    @GetMapping(value = "/new")
-    public String newArticleForm() {
-
-        return "articles/new";
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
     }
 
-    @PostMapping(value = "/posts")
-    public String createArticle(ArticleDto form) {
-        log.info(form.toString());
-        Article article = form.toEntity();
-        articleRepository.save(article);
-        return "";
+    @GetMapping("/list")
+    public String lsit(Model model){
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "list";
+    }
+
+    @GetMapping("")
+    public String index() {
+        return "redirect:/articles/list";
+    }
+
+    @GetMapping(value = "/new")
+    public String createPage() {
+        return "new";
+    }
+
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+
+        if (!optArticle.isEmpty()) {
+            // Optional.get() ---> Article
+            model.addAttribute("article", optArticle.get());
+            return "show";
+        } else {
+            return "error";
+        }
+    }
+
+    @PostMapping(value = "")
+    public String add(ArticleDto articleDto) {
+        log.info(articleDto.getTitle());
+        Article savedArticle = articleRepository.save(articleDto.toEntity());
+        log.info("generatedId:{}", savedArticle.getId());
+
+        //String.format = souf와 같음
+        return String.format("redirect:/articles/%d", savedArticle.getId());
     }
 }
