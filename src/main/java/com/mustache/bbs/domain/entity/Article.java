35161227097199d414475aedec2f7408e74333cb
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-11-08 15:38:35 +0900

    Entity 추가

--- src/main/java/com/mustache/bbs/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -2,17 +2,17 @@ package com.mustache.bbs.domain.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity //JPA에서 객체로 다루겠다는 의미
+@Table(name = "article")
 @NoArgsConstructor
 @Getter
 public class Article {
 
     @Id //PrimaryKey 역할
-    @GeneratedValue // AutoIncrease 기능 (id를 직접 입력할 경우는 제외한다)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AutoIncrease 기능 (id를 직접 입력할 경우는 제외한다)
     private Long id;
     private String title;
     private String content;
