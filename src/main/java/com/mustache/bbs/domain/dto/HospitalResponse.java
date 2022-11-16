bcc26b168a8c54b49cb593e099e5ccf7e9039f69
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-11-16 15:54:01 +0900

    HospitalResponse

---
+++ src/main/java/com/mustache/bbs/domain/dto/HospitalResponse.java
@@ -0,0 +1,40 @@
+package com.mustache.bbs.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HospitalResponse {
+    private Integer id;
+    private String roadNameAddress;
+    private String hospitalName;
+    private Integer patientRoomCount;
+    private Integer totalNumberOfBeds;
+    private String businessTypeName;
+    private Float totalAreaSize;
+    private String phoneNum;
+
+    // Service Layer
+    private String businessStatusName;
+
+
+    public HospitalResponse(Integer id, String roadNameAddress, String hospitalName, Integer patientRoomCount, Integer totalNumberOfBeds, String businessTypeName, Float totalAreaSize, String phoneNum) {
+        this.id = id;
+        this.roadNameAddress = roadNameAddress;
+        this.hospitalName = hospitalName;
+        this.patientRoomCount = patientRoomCount;
+        this.totalNumberOfBeds = totalNumberOfBeds;
+        this.businessTypeName = businessTypeName;
+        this.totalAreaSize = totalAreaSize;
+        this.phoneNum = phoneNum;
+    }
+
+    public void setBusinessStatusName(String businessStatusName) {
+        this.businessStatusName = businessStatusName;
+    }
+}
