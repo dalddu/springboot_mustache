8d67efca836a1b74e0f0cec8024effac0245b98b
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-11-16 15:53:05 +0900

    HospitalRestController

---
+++ src/main/java/com/mustache/bbs/controller/HospitalRestController.java
@@ -0,0 +1,31 @@
+package com.mustache.bbs.controller;
+
+import com.mustache.bbs.domain.dto.HospitalResponse;
+import com.mustache.bbs.domain.entity.Hospital;
+import com.mustache.bbs.repository.HospitalRepository;
+import com.mustache.bbs.service.HospitalService;
+import org.aspectj.apache.bcel.classfile.Module;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/hospitals")
+public class HospitalRestController {
+
+    private final HospitalService hospitalService;
+
+    public HospitalRestController(HospitalService hospitalService) {
+        this.hospitalService = hospitalService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<HospitalResponse> get(@PathVariable Integer id) { // ResponseEntity도 DTO타입
+        HospitalResponse hospitalResponse = hospitalService.getHospital(id); // DTO
+        return ResponseEntity.ok().body(hospitalResponse); // Return은 DTO로
+    }
+}
+
