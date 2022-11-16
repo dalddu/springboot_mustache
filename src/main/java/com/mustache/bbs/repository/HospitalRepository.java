73a20b68e3df334250d97ae187e7b0c240e196ce
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-11-16 15:52:30 +0900

    HospitalRepository

--- src/main/java/com/mustache/bbs/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs/repository/HospitalRepository.java
@@ -1,7 +1,23 @@
 package com.mustache.bbs.repository;
 
-import com.mustache.bbs.domain.Hospital;
+import com.mustache.bbs.domain.entity.Hospital;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    // JPA 기능 활용
+    // 이름(HospitalName)으로 검색
+    Page<Hospital> findByHospitalNameContaining(String searchKeyword, Pageable pageable);
+
+    // 업태구분명(businessType)으로 검색
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypesName);
+
+    // 주소(roadNameAddress) + 업태구분명(businessType)
+    List<Hospital> findByRoadNameAddressContainingAndBusinessTypeNameContaining(String roadNameAddress, String businessTypesName);
+
+    // 병상수가 특정 수 조건 사이 검색
+    List<Hospital> findByTotalNumberOfBedsBetween(Integer row, Integer high);
 }
